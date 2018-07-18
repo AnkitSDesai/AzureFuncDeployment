2e93cd8faf72359892f5f9bac6ebb0a146898b61
AnkitSDesai <ankit.das@gmail.com>
2018-07-18 14:47:06 -0400

    First commit

---
+++ f1/index.js
@@ -0,0 +1,23 @@
+module.exports = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.number) {
+        let sq = req.query.number * req.query.number;
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: {
+                square: sq,
+                num: req.query.number
+
+            }
+
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
